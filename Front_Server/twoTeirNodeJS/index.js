2b333e24f3913adc1c601da528e2d6074e3c98bb
KareemKukhon <103307695+KareemKukhon@users.noreply.github.com>
2022-07-31 00:07:49 +0300

    Update index.js

--- Front_Server/twoTeirNodeJS/index.js
+++ Front_Server/twoTeirNodeJS/index.js
@@ -2,26 +2,23 @@ const express = require('express'); //using express to creat API's
 const { text } = require('express');
 const e = require('express');
 const https = require('http');
-const cache = require('/home/front/Desktop/frontend/routeCache.js');
 const app = express();
-//var parse_Info;
-//var parse_Search;
+var parse_Info;
+var parse_Search;
 var parse_Pruchase;
-var result = []
-app.get('/CATALOG_WEBSERVICE_IP/search/:itemName', cache(300) ,(req, res) => { //search about specific topic
+app.get('/CATALOG_WEBSERVICE_IP/search/:itemName', (req, res1) => { //search about specific topic
     let data = '';
-    let parse_Search;
    //send the request for catalog server
 //send request to catalog server to give info for book due to item number
-https.get(`http://10.0.2.4:3000/CATALOG_WEBSERVICE_IP/find/${req.params.itemName}`, (res) => {//request to catalog server to check if the book exist an quantity>0
+https.get(`http://192.168.0.15:9001/CATALOG_WEBSERVICE_IP/find/${req.params.itemName}`, (res) => {//request to catalog server to check if the book exist an quantity>0
 res.on('data', (chunk) => {
     data += chunk;
 });
 res.on('end', () => {// if exist 
     //parse_Search = JSON.parse(data);
-    if(!data) { parse_Search = "the book is not exist"}
-  else {parse_Search = data}
-  result.push(parse_Search);
+  parse_Search = data
+  res1.send(parse_Search);//returend value
+
 });
 })
 
@@ -29,38 +26,36 @@ res.on('end', () => {// if exist
     console.log(error);//if theres an error
 });
 
-res.send(result);//returend value
+
 });
 
-app.get('/CATALOG_WEBSERVICE_IP/info/:itemNUM',cache(300),(req, res) => {//query to give info using item number
+app.get('/CATALOG_WEBSERVICE_IP/info/:itemNUM', (req, res1) => {//query to give info using item number
     let data = '';
-    let parse_Info;
+    //consloe.log("kareem")
     //send request to catalog server to give info for book due to item number
-    https.get(`http://10.0.2.4:3000/CATALOG_WEBSERVICE_IP/getInfo/${req.params.itemNUM}`, (res) => {//request to catalog server to check if the book exist an quantity>0
+    https.get(`http://192.168.0.15:9001/CATALOG_WEBSERVICE_IP/getInfo/${req.params.itemNUM}`, (res) => {//request to catalog server to check if the book exist an quantity>0
     res.on('data', (chunk) => {
         data += chunk;
     });
     res.on('end', () => {// if exist 
-        if(!data){parse_Info = "the book was not found"}
-        else{
-        parse_Info = JSON.parse(data);
-    const book = {
-        tittle : parse_Info.tittle,
-        quantity : parse_Info.quantity,
-        price : parse_Info.price
-    }
-        result.push(book);}
+        if(data.length === 0){
+            parse_Info = "The Book is not found"
+        }
+        else {parse_Info = JSON.parse(data); }
+        res1.send(parse_Info);//returend value
+
+     
     });
     })
     
-    console.log(result[result.length -1])
-    //console.log(R);
-    res.send(result);//returend value
+    .on('error', (error) => {
+        console.log(error);//if theres an error
+    });
     });
     
-app.get('/CATALOG_WEBSERVICE_IP/pruchase/:itemNUM',cache(300), (req, res) => {//query to buy a specific book, it will send the request to order server
+app.get('/CATALOG_WEBSERVICE_IP/pruchase/:itemNUM', (req, res1) => {//query to buy a specific book, it will send the request to order server
     let data = '';
- https.get(`http://10.0.2.7:3000/CATALOG_WEBSERVICE_IP/buy/${req.params.itemNUM}`, (res) => {//send the request to order server
+ https.get(`http://192.168.0.14:5000/CATALOG_WEBSERVICE_IP/buy/${req.params.itemNUM}`, (res) => {//send the request to order server
  res.on('data', (chunk) => {
      data += chunk;
  });
@@ -68,14 +63,15 @@ app.get('/CATALOG_WEBSERVICE_IP/pruchase/:itemNUM',cache(300), (req, res) => {//
     console.log(data)
     //parse_Pruchase = JSON.parse(data); //returned data
     parse_Pruchase = data;
+    if(!parse_Pruchase) res1.status(404).send('The Book is not found!'); //if it's null, that's mean book not available
+    else {res1.send("Bought successfuly!");}//if exist return bought list
+   
  });
  })
  .on('error', (error) => {
      console.log(error);
  });
- if(!parse_Pruchase) res.status(404).send('The Book is not found!'); //if it's null, that's mean book not available
- else {res.send("Bought successfuly!");}//if exist return bought list
 });
 
-const port = process.env.PORT||3000;//port listining to request
+const port = process.env.PORT||5000;//port listining to request
 app.listen(port, () => console.log(` Listeningon port $ { port }...`))
