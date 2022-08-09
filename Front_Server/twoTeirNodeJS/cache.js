1ecf309364ee08758357bead654a70a1194bb0ba
KareemKukhon <103307695+KareemKukhon@users.noreply.github.com>
2022-08-10 02:12:23 +0300

    Update cache.js

--- Front_Server/twoTeirNodeJS/cache.js
+++ Front_Server/twoTeirNodeJS/cache.js
@@ -27,142 +27,177 @@ app.get('/CATALOG_WEBSERVICE_IP/cachefind/:ItemName', (req, res1) => {
  
     let data = '';
     let notfound = '';
-    console.log(req.params.ItemName)
-    let Book = catalog.filter((c => c.topic === req.params.ItemName));
-    let result = Book.map(o => ({ tittle: o.tittle, quantity: parseInt(o.quantity), price: parseInt(o.price), topic: o.topic }));
-    if (result.length === 0){
-        if(flag1 === 1){
-        https.get(`http://192.168.0.15:5000/CATALOG_WEBSERVICE_IP/find/${req.params.ItemName}`, (res) => {//request to catalog server to check if the book exist an quantity>0
-        res.on('data', (chunk) => {
-            data += chunk;
-        });
-        res.on('end', () => {// if exist 
-            if(data.length === 0){
-                notfound = "book not found"
-                return res1.status(404).send(notfound);
-            }
-            else {
-                
-                parse_Info = JSON.parse(data.toString());
-                //console.log(parse_Info.title)
-                flag1 = 0;
-                console.log(flag1);
-                for(let i=0; i<parse_Info.length;i++){
-                const book = {
-                    id : parse_Info[i].id,
-                    price : parse_Info[i].price,
-                    tittle : parse_Info[i].tittle,
-                    quantity : parse_Info[i].quantity,
-                    topic : parse_Info[i].topic
-    
-                }
-                catalog.push(book)
-               console.log(book)
-            
-                const csvWriter = createCsvWriter({
-                    path: 'cache.csv',
-                    header: [
-                        { id: 'id', title: 'id' },
-                        { id: 'price', title: 'price' },
-                        { id: 'tittle', title: 'tittle' },
-                        { id: 'quantity', title: 'quantity' },
-                        { id: 'topic', title: 'topic' }
-                    ]
-                });
-                csvWriter
-                    .writeRecords(catalog)
-                    .then(() => console.log(''));
-            }
-                  return res1.send(parse_Info);//returend value 
-                  
-
-            }
-            //res1.send(parse_Info);
-            //res1.setHeader('Content-Type', 'text/html');
-        });
-        })
-        .on('error', (error) => {
-            console.log(error);//if theres an error
-        })
+    let count = '';
+    let count1 = 0;
+    https.get(`http://192.168.0.15:5000/CATALOG_WEBSERVICE_IP/${req.params.ItemName}`, (res2) => {//request to catalog server to check if the book exist an quantity>0
+    res2.on('data', (chunk) => {
+        notfound += chunk;
+        count = notfound;
+        console.log(notfound)
         
-    }
-
-    if(flag1 === 0){
-        https.get(`http://192.168.0.14:5000/CATALOG_WEBSERVICE_IP/find/${req.params.ItemName}`, (res2) => {//request to catalog server to check if the book exist an quantity>0
-        res2.on('data', (chunk) => {
-            data += chunk;
-        });
-        res2.on('end', () => {// if exist 
-            if(data.length === 0){
-                notfound = "book not found"
-                return res1.status(404);
+    });
+    res2.on('end', () => {// if exist 
+         count = notfound;
+         for(var i in catalog){
+            if(catalog[i].topic === req.params.ItemName){
+                count1++;
             }
-            else {
-                parse_Info = JSON.parse(data.toString());
-                flag1 = 1;
-                for(let i=0; i<parse_Info.length;i++){
+        }
+
+        let Book = catalog.filter((c => c.topic === req.params.ItemName));
+        let result = Book.map(o => ({ id: o.id, tittle: o.tittle, quantity: parseInt(o.quantity), price: parseInt(o.price), topic: o.topic }));
+        if ((result.length === 0) || (count != count1.toString())){
+            if(flag1 === 1){
+            https.get(`http://192.168.0.15:5000/CATALOG_WEBSERVICE_IP/find/${req.params.ItemName}`, (res) => {//request to catalog server to check if the book exist an quantity>0
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {// if exist 
+                if(data === "0"){
+                    flag1 = 0;
+                    return res1.status(404).send("0");
+                }
+                else {
+                    
+                    parse_Info = JSON.parse(data);
+                    flag1 = 0;
+                    console.log(flag1);
+                    for(let i=0; i<parse_Info.length;i++){
                     const book = {
                         id : parse_Info[i].id,
                         price : parse_Info[i].price,
                         tittle : parse_Info[i].tittle,
                         quantity : parse_Info[i].quantity,
                         topic : parse_Info[i].topic
-        
                     }
+                    
+                    for(var j in catalog){
+                    if(catalog[j].id.toString() === book.id.toString() ){
+                    catalog.splice(j, 1)
+                    
+                    break;
+                    }
+                }
                 catalog.push(book)
+                   console.log(book)
                 
-                const csvWriter = createCsvWriter({
-                    path: 'cache.csv',
-                    header: [
-                        { id: 'id', title: 'id' },
-                        { id: 'price', title: 'price' },
-                        { id: 'tittle', title: 'tittle' },
-                        { id: 'quantity', title: 'quantity' },
-                        { id: 'topic', title: 'topic' }
-                    ]
-                });
-                csvWriter
-                    .writeRecords(catalog)
-                    .then(() => console.log(''));
-            }
-                 return res1.send(parse_Info);//returend value 
-                
+                    const csvWriter = createCsvWriter({
+                        path: 'cache.csv',
+                        header: [
+                            { id: 'id', title: 'id' },
+                            { id: 'price', title: 'price' },
+                            { id: 'tittle', title: 'tittle' },
+                            { id: 'quantity', title: 'quantity' },
+                            { id: 'topic', title: 'topic' }
+                        ]
+                    });
+                    csvWriter
+                        .writeRecords(catalog)
+                        .then(() => console.log(''));
+                }
+                      return res1.send(parse_Info);//returend value 
+                      
+    
+                }
+            });
+            })
+            .on('error', (error) => {
+                console.log(error);//if theres an error
+            })
+            
+        }
+    
+        if(flag1 === 0){
+            https.get(`http://192.168.0.14:5000/CATALOG_WEBSERVICE_IP/find/${req.params.ItemName}`, (res2) => {//request to catalog server to check if the book exist an quantity>0
+            res2.on('data', (chunk) => {
+                data += chunk;
+            });
+            res2.on('end', () => {// if exist 
+                if(data === "0"){
+                    flag1=1;
+                    return res1.status(404).send("0");
+                }
+                else {
+                    parse_Info = JSON.parse(data.toString());
+                    flag1 = 1;
+                    for(let i=0; i<parse_Info.length;i++){
+                        const book = {
+                            id : parse_Info[i].id,
+                            price : parse_Info[i].price,
+                            tittle : parse_Info[i].tittle,
+                            quantity : parse_Info[i].quantity,
+                            topic : parse_Info[i].topic
+            
+                        }
+                        for(var j in catalog){
+                            if(catalog[j].id.toString() === book.id.toString() ){
+                            catalog.splice(j, 1)
+                            
+                            break;
+                            }
+                        }
+                    catalog.push(book)
+                    
+                    const csvWriter = createCsvWriter({
+                        path: 'cache.csv',
+                        header: [
+                            { id: 'id', title: 'id' },
+                            { id: 'price', title: 'price' },
+                            { id: 'tittle', title: 'tittle' },
+                            { id: 'quantity', title: 'quantity' },
+                            { id: 'topic', title: 'topic' }
+                        ]
+                    });
+                    csvWriter
+                        .writeRecords(catalog)
+                        .then(() => console.log(''));
+                }
+                     return res1.send(parse_Info);//returend value 
+                    
+    
+                }
+                 
+            });
+            })
+            .on('error', (error) => {
+                console.log(error);//if theres an error
+            })
+            
+        }
+        }
+        else{
+             res1.send(result);
+    }
 
-            }
-             
-        });
-        })
-        .on('error', (error) => {
-            console.log(error);//if theres an error
-        })
-        
-    }
-    //res1.send(parse_Info);
-    }
-    else{
-         res1.send(result);
-}
+    });
+
+});
+
 });
 
 app.get('/CATALOG_WEBSERVICE_IP/getInfo/:itemNUM', (req, res1) => {
     let data = '';
     let notfound = '';
+    
     let Book = catalog.filter((c => c.id === req.params.itemNUM));
     //console.log(Book)
     let result = Book.map(o => ({ tittle: o.tittle, quantity: parseInt(o.quantity), price: parseInt(o.price), topic: o.topic }));
     if (result.length === 0){
+
         if(f === 1){
+            
         https.get(`http://192.168.0.15:5000/CATALOG_WEBSERVICE_IP/getInfo/${req.params.itemNUM}`, (res) => {//request to catalog server to check if the book exist an quantity>0
         res.on('data', (chunk) => {
             data += chunk;
         });
         res.on('end', () => {// if exist 
-            if(data.length === 0){
-                notfound = "book not found"
-                return res1.status(404);
+            if(data.toString() === "0"){
+                f=0;
+                return res1.status(404).send("0");
+                
             }
             else {
-                parse_Info = JSON.parse(data.toString());
+                parse_Info = (data.toString());
                 f = 0;
                 const book = {
                     id : req.params.itemNUM,
@@ -204,9 +239,9 @@ app.get('/CATALOG_WEBSERVICE_IP/getInfo/:itemNUM', (req, res1) => {
             data += chunk;
         });
         res.on('end', () => {// if exist
-            if(data.length === 0){
-                notfound = "book not found"
-                return res1.status(404);
+            if(data === "0"){
+                f=1;
+                return res1.status(404).send("0");
             }
             else {
                 parse_Info = JSON.parse(data.toString());
@@ -251,6 +286,7 @@ app.get('/CATALOG_WEBSERVICE_IP/getInfo/:itemNUM', (req, res1) => {
          res1.send(result[0]);
 }
 });
+
 app.get('/CATALOG_WEBSERVICE_IP/put/:itemNUM', (req, res1) => {
     let data = '';
     if(ord === 1){
@@ -259,10 +295,10 @@ app.get('/CATALOG_WEBSERVICE_IP/put/:itemNUM', (req, res1) => {
      data += chunk;
  });
  res.on('end', () => {
-    console.log(data)
+
     parse_Pruchase = data;
     
-    if(!parse_Pruchase) res1.status(404).send('The Book is not found!'); //if it's null, that's mean book not available
+    if(!parse_Pruchase === "0") res1.status(404).send("0"); //if it's null, that's mean book not available
     else {
     let Book = catalog.filter((c => c.id === req.params.itemNUM));
     if (!Book) return res1.status(404).send('The Book is not found!');
@@ -289,6 +325,7 @@ app.get('/CATALOG_WEBSERVICE_IP/put/:itemNUM', (req, res1) => {
                     }
                 }
             }
+            ord = 0;
             return res1.send("Bought successfuly!");
         }//if exist return bought list
         }
@@ -300,7 +337,7 @@ app.get('/CATALOG_WEBSERVICE_IP/put/:itemNUM', (req, res1) => {
  .on('error', (error) => {
     console.log(error);
 });
-ord = 0;
+
 }
 if(ord === 0){
  https.get(`http://192.168.0.15:5001/CATALOG_WEBSERVICE_IP/buy/${req.params.itemNUM}`, (res) => {//send the request to order server
@@ -308,7 +345,7 @@ if(ord === 0){
      data += chunk;
  });
  res.on('end', () => {
-    console.log(data)
+
     parse_Pruchase = data;
     if(!parse_Pruchase) res1.status(404).send('The Book is not found!'); //if it's null, that's mean book not available
     else {
@@ -337,6 +374,7 @@ if(ord === 0){
                     }
                 }
             }
+            ord = 1;
             res1.send("Bought successfuly!");}//if exist return bought list
         }
         
@@ -347,7 +385,7 @@ if(ord === 0){
  .on('error', (error) => {
      console.log(error);
  });
- ord = 1;
+
 }
 });
 
