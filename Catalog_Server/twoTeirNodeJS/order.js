b45ae9bb6a33c9a398dce4b4f5796062b062db56
KareemKukhon <103307695+KareemKukhon@users.noreply.github.com>
2022-08-10 02:14:36 +0300

    Update order.js

--- Catalog_Server/twoTeirNodeJS/order.js
+++ Catalog_Server/twoTeirNodeJS/order.js
@@ -16,7 +16,7 @@ const { text, response } = require('express');
 const e = require('express');
 
 const app = express();
-app.post('/CATALOG_WEBSERVICE_IP/buy/:itemNUM', (req, res) => {
+app.get('/CATALOG_WEBSERVICE_IP/buy/:itemNUM', (req, res1) => {
    // let Book = catalog.filter((c => c.id === req.params.itemNUM));
    // if (!Book) res.status(404).send('The Book is not found!');
    let data = '';
@@ -24,13 +24,14 @@ app.post('/CATALOG_WEBSERVICE_IP/buy/:itemNUM', (req, res) => {
 https.get(`http://localhost:5000/CATALOG_WEBSERVICE_IP/put/${req.params.itemNUM}`, (res) => {
 res.on('data', (chunk) => {
     data += chunk;
-    console.log(chunk);
 });
 res.on('end', () => {
-    console.log(data);
- parse = JSON.parse(data);
+if(data.toString() === "0"){
+    return res1.status(404).send("0")
+} 
+parse = JSON.parse(data);
  const book = {
-    id : Book_Sold.length,
+    id : Book_Sold.length +1,
     tittle : parse.tittle,
     price: parse.price
  } 
@@ -57,7 +58,7 @@ csvWriterOrder
 https.get(`http://192.168.0.14:5000/CATALOG_WEBSERVICE_IP/put/${req.params.itemNUM}`, (res) => {
 
 })
-return res.send(Book_Sold);
+return res1.send(Book_Sold);
 });
 
 const port = process.env.PORT || 5001;
